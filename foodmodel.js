b527c7d1ccb1874a75533c28aeef8af1edefd75d
Miwtoo <miwyugioh@gmail.com>
2021-01-01 19:11:27 +0700

    miw

--- foodmodel.js
+++ foodmodel.js
@@ -1,21 +1,78 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
-var foodSchema = mongoose.Schema(
-  {
-    // กำหนด ชื่อและชนิดของ document เรา
-    name: {
-      type: String
-    },
-    price: {
-      type: Number
-    }
-  },
-  {
-    // กำหนด collection ของ MongoDB หรือจะไม่กำหนดก็ได้
-    collection: "FOODS"
-  }
+var foodSchema = new mongoose.Schema(
+	{
+		// กำหนด ชื่อและชนิดของ document เรา
+		name: {
+			type: String,
+		},
+		lastname: {
+			type: String,
+		},
+		tel: {
+			type: Number,
+		},
+		building: {
+			type: String,
+		},
+		tel2: {
+			type: Number,
+		},
+		buildingNo: {
+			type: String,
+		},
+		Floor: {
+			type: Number,
+		},
+		Room: {
+			type: Number,
+		},
+		isp: {
+			type: String,
+		},
+		ISPCode: {
+			type: Number,
+		},
+		MemberNumber: {
+			type: Number,
+		},
+		Fees: {
+			type: Number,
+		},
+		team: {
+			type: String,
+		},
+		Confirming: {
+			type: String,
+		},
+		lastDate: {
+			type: Date,
+		},
+		time: {
+			type: Number,
+		},
+		Remark: {
+			type: String,
+		},
+		Status: {
+			type: String,
+		},
+		SubStatus: {
+			type: String,
+		},
+		DateConnect: {
+			type: Date,
+		},
+		DateDisconnect: {
+			type: Date,
+		},
+	},
+	{
+		// กำหนด collection ของ MongoDB หรือจะไม่กำหนดก็ได้
+		collection: 'FOODS',
+	}
 );
 
 // ถ้าไม่ได้กำหนด collection ข้างบน default จะเป็น "foods"
-var Food = mongoose.model("foods", foodSchema);
+var Food = mongoose.model('foods', foodSchema);
 module.exports = Food;
